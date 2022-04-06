c777d166bbd963ec90afa79f70674076967fb24e
tapia1013 <manbearpig1013@gmail.com>
2022-04-06 14:59:07 -0700

    logic to show products or if its loading or error if its an error and useSelector and usedispatch

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -25,16 +25,27 @@ const HomeScreen = () => {
     dispatch(listProducts())
   }, [dispatch])
 
+
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+
+      {loading ?
+        (<h2>Loading...</h2>)
+        : error
+          ? (<h3>{error}</h3>)
+          : (
+            <Row>
+              {products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              ))}
+            </Row>
+          )
+      }
+
+
     </>
   )
 }
