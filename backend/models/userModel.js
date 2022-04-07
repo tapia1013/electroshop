81ad16c1c71f561382d816b5bc461b55036144f7
tapia1013 <manbearpig1013@gmail.com>
2022-04-07 14:39:33 -0700

    register user backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,6 +30,18 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+// encrypt password before we save below
+userSchema.pre('save', async function (next) {
+  // Only do this if password field is set
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  // hash password with method in bcrypt
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
