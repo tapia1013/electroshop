cb980cd4a605fc5857f978794df8da0aeb1f2fc8
tapia1013 <manbearpig1013@gmail.com>
2022-04-07 17:29:22 -0700

    show user in navbar with dropdown to logout and logout functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import {
   USER_LOGIN_FAIL,
-  USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 
 } from "../constants/userConstants"
 
@@ -43,4 +45,13 @@ export const login = (email, password) => async (dispatch) => {
         : error.message
     })
   }
+}
+
+
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  dispatch({
+    type: USER_LOGOUT
+  })
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { logout } from '../actions/userActions';
+
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
+
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar bg='light' variant='light' expand='lg' collapseOnSelect>
@@ -21,16 +34,34 @@ const Header = () => {
                 </Nav.Link>
               </LinkContainer>
 
-              <LinkContainer to='/login'>
-                <Nav.Link style={{ fontSize: '0.8rem', fontWeight: '700' }} href='/login'>
-                  <i className='fas fa-user'></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
+              {
+                userInfo
+                  ? (
+                    <NavDropdown title={userInfo.name} id='username'>
+                      <LinkContainer to='/profile'>
+                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                      </LinkContainer>
+
+                      <NavDropdown.Item onClick={logoutHandler}>
+                        Logout
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                  )
+                  : (
+                    <LinkContainer to='/login'>
+                      <Nav.Link style={{ fontSize: '0.8rem', fontWeight: '700' }} href='/login'>
+                        <i className='fas fa-user'></i> Sign In
+                      </Nav.Link>
+                    </LinkContainer>
+                  )
+              }
+
+
             </Nav>
           </Navbar.Collapse>
         </Container>
-      </Navbar>
-    </header>
+      </Navbar >
+    </header >
   )
 }
 
