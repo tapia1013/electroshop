b1414371d8e5704ce9922cc0df9918ec45091049
tapia1013 <manbearpig1013@gmail.com>
2022-04-08 19:16:18 -0700

    fixed order detail reducers to be loading true initially

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -38,7 +38,7 @@ export const orderCreateReducer = (state = {}, action) => {
 
 
 // ...state keeps us from getting errors
-export const orderDetailsReducer = (state = { orderItems: [], shippingAddress: {} }, action) => {
+export const orderDetailsReducer = (state = { loading: true, orderItems: [], shippingAddress: {} }, action) => {
   switch (action.type) {
     case ORDER_DETAILS_REQUEST:
       return {
