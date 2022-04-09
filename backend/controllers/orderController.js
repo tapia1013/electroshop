2834a5374433bf2e588c6e0c391da8809c73ac2c
tapia1013 <manbearpig1013@gmail.com>
2022-04-08 17:44:10 -0700

    order controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,44 @@
+import asyncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js';
+
+
+// @desc     Create new order
+// @route    POST /api/orders
+// @access   Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice
+  } = req.body
+
+  // Make sure order items is not empty
+  if (orderItems && orderItems.length === 0) {
+    res.status(400)
+    throw new Error('No Order Items')
+    return;
+  } else {
+    // create new order in the database
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice
+    })
+
+    // save in database
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder)
+  }
+
+
+})
