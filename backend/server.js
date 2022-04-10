90885708acd99ddce734a9e14470f5b330758087
tapia1013 <manbearpig1013@gmail.com>
2022-04-09 20:33:27 -0700

    indenting

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import colors from 'colors'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
@@ -8,38 +7,27 @@ import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 
-
 dotenv.config()
-
 
 connectDB()
 
-
 const app = express();
-
 
 // bodyParser replacement
 app.use(express.json())
-
-
 
 app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
-
-
 
 // NotFound Middleware
 app.use(notFound)
 // Error Middleware
 app.use(errorHandler)
-
 
 const PORT = process.env.PORT || 8000
 
